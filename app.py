6ea697ecee507f41f274f9273f1937ff378d3d94
DAGGUBATI SUJAY <sujaydaggubati1@gmail.com>
2024-04-21 16:48:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -194,12 +194,12 @@ def load_model():
 
 model = load_model()
 
+st.set_page_config(
+    page_title="Forecasting Product Returns",
+    # page_icon="logo.png",  # URL to the logo image
+    layout="wide"
+)
 
-# st.set_page_config(
-#     page_title="Forecasting Product Returns",
-#     # page_icon="logo.png",  # URL to the logo image
-#     layout="wide"
-# )
 
 def display_risk_message(risk, probability):
     if risk == "Low":
@@ -218,6 +218,7 @@ def display_risk_message(risk, probability):
     bg_color = bg_colors[risk]
     text_color = text_colors[risk]
 
+    
     
     # Using markdown and inline CSS to style the output
     st.markdown(f"""
